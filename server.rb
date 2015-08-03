ddeb5731aacb3632f5ce2ab14cf0cfee70bc09eb
Andy Brown <brownie3003@gmail.com>
2015-08-03 17:26:20 +0100

    format ruby

--- server.rb
+++ server.rb
@@ -19,22 +19,22 @@ root = File.expand_path './public'
 server = WEBrick::HTTPServer.new :Port => port, :DocumentRoot => root
 
 server.mount_proc '/comments.json' do |req, res|
-  comments = JSON.parse(File.read('./comments.json'))
-
-  if req.request_method == 'POST'
-    # Assume it's well formed
-    comment = {}
-    req.query.each do |key, value|
-      comment[key] = value.force_encoding('UTF-8')
+    comments = JSON.parse(File.read('./comments.json'))
+    
+    if req.request_method == 'POST'
+        # Assume it's well formed
+        comment = {}
+        req.query.each do |key, value|
+            comment[key] = value.force_encoding('UTF-8')
+        end
+        comments << comment
+        File.write('./comments.json', JSON.pretty_generate(comments, :indent => '    '))
     end
-    comments << comment
-    File.write('./comments.json', JSON.pretty_generate(comments, :indent => '    '))
-  end
-
-  # always return json
-  res['Content-Type'] = 'application/json'
-  res['Cache-Control'] = 'no-cache'
-  res.body = JSON.generate(comments)
+    
+    # always return json
+    res['Content-Type'] = 'application/json'
+    res['Cache-Control'] = 'no-cache'
+    res.body = JSON.generate(comments)
 end
 
 trap 'INT' do server.shutdown end
